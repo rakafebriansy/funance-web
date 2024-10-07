ba273e5369be76c55a6da1ff6cf58ddd1aa8492b
rakafebriansy <kerenr445@gmail.com>
2024-10-07 14:12:30 +0700

    feat: adding close and link button into sign in page in sign up

--- src/components/elements/PrimaryButton.jsx
+++ src/components/elements/PrimaryButton.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 
-const PrimaryButton = ({ btnType = 'button', small, children }) => {
+const PrimaryButton = ({ btnType = 'button', small, children, outlined = false }) => {
     if(btnType == 'link') {
         return (
-            <Link className={`flex bg-primary w-full relative font-semibold mb-1 text-white rounded-full justify-center items-center ${small ? 'p-1 text-sm' : 'p-2 text-base'}`}>
-                <div className="w-full h-full absolute left-0 bg-dark top-1 rounded-full z-[-1]"></div>
+            <Link className={`flex w-full relative font-semibold mb-1 rounded-full justify-center items-center ${small ? 'p-1 text-sm' : 'p-2 text-base'} ${outlined ? 'bg-white text-primary' : 'bg-primary text-white'}`}>
+                <div className={`w-full h-full absolute left-0 top-1 rounded-full z-[-1] ${outlined ? 'bg-[#E5E5E5]' : 'bg-dark'}`}></div>
                 <span>{children}</span>
             </Link>
         );
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -7,7 +7,29 @@ import waveImg from '../assets/images/wave.png'
 
 const SignUp = () => {
     return (
-        <div className="w-full h-screen flex justify-center items-center">
+        <div className="w-full h-screen flex justify-center items-center relative">
+            <div className="w-20 absolute left-5 top-5">
+                <button className='w-5 h-5'>
+                    <svg className="h-full w-full" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g clip-path="url(#clip0_171_1377)">
+                    <g clip-path="url(#clip1_171_1377)">
+                    <path d="M2 2L14 14M14 2L2 14" stroke="#B1B1B1" stroke-width="3" stroke-linecap="round"/>
+                    </g>
+                    </g>
+                    <defs>
+                    <clipPath id="clip0_171_1377">
+                    <rect className="h-full w-full" fill="white"/>
+                    </clipPath>
+                    <clipPath id="clip1_171_1377">
+                    <rect className="h-full w-full" fill="white"/>
+                    </clipPath>
+                    </defs>
+                    </svg>
+                </button>
+            </div>
+            <div className="w-20 absolute right-5 top-5">
+                <PrimaryButton btnType="link" small={false} outlined={true}>MASUK</PrimaryButton>
+            </div>
             <form className="flex flex-col items-center w-96 gap-4">
                 <h1 className='text-2xl font-bold'>Buat Akunmu</h1>
                 <ul className='flex flex-col gap-2 w-full'>
